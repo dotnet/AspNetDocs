5a2577203470c8a46c83f58558a63c7e33096809
Wade Pickett <wpickett@microsoft.com>
2021-03-24 15:06:59 -0700

    Update aspnet/web-api/overview/security/individual-accounts-in-web-api/samples/sample5.cmd
    Co-authored-by: Rick Anderson <3605364+Rick-Anderson@users.noreply.github.com>

--- aspnet/web-api/overview/security/individual-accounts-in-web-api/samples/sample5.cmd
+++ aspnet/web-api/overview/security/individual-accounts-in-web-api/samples/sample5.cmd
@@ -7,4 +7,4 @@ X-Requested-With: XMLHttpRequest
 Referer: https://localhost:44305/
 Content-Length: 84
 
-{"Email":"alice@example.com",$CREDENTIAL_PLACEHOLDER`$,$CREDENTIAL_PLACEHOLDER2$"}
+{"Email":"alice@example.com",$CREDENTIAL_PLACEHOLDER1$,$CREDENTIAL_PLACEHOLDER2$"}
