ba3be784541e86ca7faad9fbe5257ba87acddbc5
Wade Pickett <wpickett@microsoft.com>
2021-03-24 12:01:35 -0700

    Update aspnet/web-forms/overview/deployment/advanced-enterprise-web-deployment/performing-a-what-if-deployment/samples/sample1.cmd
    performing-a-what-if-deployment/samples/sample1.cmd changed back to original

--- aspnet/web-forms/overview/deployment/advanced-enterprise-web-deployment/performing-a-what-if-deployment/samples/sample1.cmd
+++ aspnet/web-forms/overview/deployment/advanced-enterprise-web-deployment/performing-a-what-if-deployment/samples/sample1.cmd
@@ -4,7 +4,7 @@ MSDeploy.exe
   -dest:auto,
         computerName="https://stageweb1:8172/MSDeploy.axd?site=DemoSite",
         username="FABRIKAM\stagingdeployer",
-        password=$CREDENTIAL_PLACEHOLDER$,
+        password="Pa$$w0rd",
         authtype="Basic",
         includeAcls="False"
   -verb:sync
