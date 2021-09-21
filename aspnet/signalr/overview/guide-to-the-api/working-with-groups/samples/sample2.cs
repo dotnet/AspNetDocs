1410d400fa224e77ae8a31f8022e1712c6eb7e55
Sujish Pradeep <50685015+sujishpradeep@users.noreply.github.com>
2021-09-21 11:10:07 +1000

    update async call in signalR add to group sample

--- aspnet/signalr/overview/guide-to-the-api/working-with-groups/samples/sample2.cs
+++ aspnet/signalr/overview/guide-to-the-api/working-with-groups/samples/sample2.cs
@@ -1,5 +1,5 @@
 public async Task JoinRoom(string roomName)
 {
-    await Groups.Add(Context.ConnectionId, roomName);
+    await Groups.AddToGroupAsync(Context.ConnectionId, roomName);
     Clients.Group(roomName).addChatMessage(Context.User.Identity.Name + " joined.");
 }
